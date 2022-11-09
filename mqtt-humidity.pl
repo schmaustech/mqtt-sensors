3a0a139f9462e67407c6663cc623be3de19ccdad
Ben Schmaus <bschmaus@redhat.com>
2022-11-09 15:32:52 -0600

    Update mqtt-humidity.pl

--- mqtt-humidity.pl
+++ mqtt-humidity.pl
@@ -3,46 +3,59 @@ use strict;
 use warnings;
 use JSON qw(decode_json);
 use Net::MQTT::Simple;
-use Data::Dumper;
-use Getopt::Std;
+use Getopt::Long 'HelpMessage';
 my($temperature,$humidity,$fahrenheit);
 my $dehumidifier="off";
 
-my %options=();
-getopts("i:p:h", \%options);
+GetOptions(
+  'hostname=s' => \ my $hostname,
+  'port=i'   => \(my $port = '1883'),
+  'threshold=i'   => \(my $threshold = '60'),
+  'help'     =>   sub { HelpMessage(0) },
+) or HelpMessage(1);
 
-sub printhelp {
-  print "mqtt-example.pl -i <mqtthostip> -p <port>  -h <help>\n";
-  exit 0;
-}
+HelpMessage(1) unless $hostname;
 
-if (defined $options{h}) {
-  printhelp();
-}
+mqtt_interact();
 
-if (defined $options{i} && defined $options{p}) {
-  my $mqtt = Net::MQTT::Simple->new("$options{i}:$options{p}");
+exit(0);
+
+sub mqtt_interact {
+  my $mqtt = Net::MQTT::Simple->new("$hostname:$port");
   $mqtt->publish('MQTT-Perl-Monitor' => 'Humidity monitor has subscribed and listenting...');
   $mqtt->run(
     "zigbee2mqtt/Temperature Humidity Sensor" => sub {
        my ($topic, $message) = @_;
        my $decoded = decode_json($message);
-       # print Dumper($decoded);
        $humidity = $decoded->{'humidity'};
        $temperature = $decoded->{'temperature'};
        $fahrenheit = (9 * $temperature/5) + 32;
        print "Temp C = $temperature : Temp F = $fahrenheit : Humidity = $humidity\n";
-       if ($humidity >= 60 && $dehumidifier eq "off") {
+       if ($humidity >= $threshold && $dehumidifier eq "off") {
          $mqtt->publish('zigbee2mqtt/Smart Outlet/set' => '{ "state": "ON" }');
          $dehumidifier="on"
        }
-       if ($humidity < 60 && $dehumidifier eq "on") {
+       if ($humidity < $threshold && $dehumidifier eq "on") {
          $mqtt->publish('zigbee2mqtt/Smart Outlet/set' => '{ "state": "OFF" }');
          $dehumidifier="off"
        }
     },
   );
-} else {
-  printhelp();
 }
-exit;
+
+=head1 NAME
+
+mqtt-humidity.pl - Monitor MQTT queue humidity and take action by turning on or off smart outlet
+
+=head1 SYNOPSIS
+
+  --hostname,-h   Hostname or IP address of MQTT host
+  --port,-p       Port for MQTT (defaults to default 1883)
+  --threshold,-t  Threshold for humidity (defaults to 50%)
+  --help,-h       Print this help
+
+=head1 VERSION
+
+0.01
+
+=cut
